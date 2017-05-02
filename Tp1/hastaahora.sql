f86ce4678820291f5289d5c830c791becb74f990
jnvillar <juan.villar@mercadolibre.com>
2017-05-02 19:36:38 -0300

    merge mis ejercicios, ejercicios del resto

--- Tp1/hastaahora.sql
+++ Tp1/hastaahora.sql
@@ -1,14 +1,15 @@
 CREATE TABLE Maestro (
-  	NroDePlacaMaestro integer NOT NULL,
+    NroDePlacaMaestro integer NOT NULL,
     Nombre varchar(255) NOT NULL,
-		Apellido varchar(255) NOT NULL,
-		Graduacion varchar(255) NOT NULL,
+        Apellido varchar(255) NOT NULL,
+        Graduacion varchar(255) NOT NULL,
     PRIMARY KEY (NroDePlacaMaestro),
     CHECK (Graduacion IN ("Primer Dan", "Segundo Dan", "Tercer Dan", "Cuarto Dan", "Quinto Dan", "Sexto Dan"))
 );
 
 CREATE TABLE Escuela (
     IdEscuela integer NOT NULL,
+    Nombre varchar(255) NOT NULL,
     Pais varchar(255) NOT NULL,
     NroDePlacaMaestro integer NOT NULL,
     PRIMARY KEY (IdEscuela),
@@ -18,10 +19,10 @@ CREATE TABLE Escuela (
 CREATE TABLE Alumno (
     DNI integer NOT NULL,
     Nombre varchar(255) NOT NULL,
-		Apellido varchar(255) NOT NULL,
-		NroCertificadoITF integer NOT NULL,
-		Graduacion varchar(255) NOT NULL,
-		Foto varchar(255) NOT NULL,
+        Apellido varchar(255) NOT NULL,
+        NroCertificadoITF integer NOT NULL,
+        Graduacion varchar(255) NOT NULL,
+        Foto varchar(255) NOT NULL,
     PRIMARY KEY (DNI),
     CHECK (Graduacion IN ("Primer Dan", "Segundo Dan", "Tercer Dan", "Cuarto Dan", "Quinto Dan", "Sexto Dan"))
 );
@@ -41,14 +42,14 @@ CREATE TABLE Coach (
 CREATE TABLE Competidor (
     DNI integer NOT NULL,
     Peso integer NOT NULL,
-		Sexo char(1) NOT NULL,
-		FechaDeNacimiento date NOT NULL,
+        Sexo char(1) NOT NULL,
+        FechaDeNacimiento date NOT NULL,
     IdEquipo integer,
-  	DNICoach integer NOT NULL,
+    DNICoach integer NOT NULL,
     PRIMARY KEY (DNI),
     FOREIGN KEY (IdEquipo) REFERENCES Equipo(IdEquipo),
     FOREIGN KEY (DNI) REFERENCES Alumno(DNI),
-  	FOREIGN KEY (DNICoach) REFERENCES Coach(DNI)
+    FOREIGN KEY (DNICoach) REFERENCES Coach(DNI)
 );
 
 CREATE TABLE Modalidad (
@@ -57,7 +58,7 @@ CREATE TABLE Modalidad (
     Sexo char(1) NOT NULL,
     PRIMARY KEY (IdModalidad),
     CHECK (Tipo IN ("Combate", "Forma", "Salto", "Rotura Potencia", "Combate Equipos")),
-  	CHECK (Sexo IN ('F', 'M')) 
+    CHECK (Sexo IN ('F', 'M')) 
 );
 
 CREATE TABLE Inscripcion (
@@ -67,7 +68,7 @@ CREATE TABLE Inscripcion (
     PRIMARY KEY (IdModalidad,DNI),
     FOREIGN KEY (IdModalidad) REFERENCES Modalidad(IdModalidad),
     FOREIGN KEY (DNI) REFERENCES Competidor(DNI),
- 	  FOREIGN KEY (NroDePlacaMaestro) REFERENCES Maestro(NroDePlacaMaestro)
+      FOREIGN KEY (NroDePlacaMaestro) REFERENCES Maestro(NroDePlacaMaestro)
 );
 
 CREATE TABLE Combate (
@@ -76,8 +77,8 @@ CREATE TABLE Combate (
     Edad varchar(255) NOT NULL,
     PRIMARY KEY (IdModalidad),
     FOREIGN KEY (IdModalidad) REFERENCES Modalidad(IdModalidad),
-  	CHECK (Peso IN ("Liviano", "Medio", "Pesado") ),
-  	CHECK (Edad IN ("Juveniles", "Adultos") )
+    CHECK (Peso IN ("Liviano", "Medio", "Pesado") ),
+    CHECK (Edad IN ("Juveniles", "Adultos") )
 );
 
 CREATE TABLE Forma (
@@ -116,11 +117,11 @@ CREATE TABLE Competencia (
 );
 
 CREATE TABLE Arbitro (
-		NroPlacaDeArbitro integer NOT NULL,
+        NroPlacaDeArbitro integer NOT NULL,
     Nombre varchar(255) NOT NULL,
-		Apellido varchar(255) NOT NULL,
-		Pais varchar(255) NOT NULL,
-		Graduacion varchar(255) NOT NULL,
+        Apellido varchar(255) NOT NULL,
+        Pais varchar(255) NOT NULL,
+        Graduacion varchar(255) NOT NULL,
     PRIMARY KEY (NroPlacaDeArbitro),
     CHECK (Graduacion IN ("Primer Dan", "Segundo Dan", "Tercer Dan", "Cuarto Dan", "Quinto Dan", "Sexto Dan"))
 );
@@ -159,9 +160,9 @@ INSERT INTO Maestro VALUES (1, 'Pepe', 'Argento', 'Primer Dan');
 INSERT INTO Maestro VALUES (2, 'Armando', 'Barreda', 'Segundo Dan');
 INSERT INTO Maestro VALUES (3, 'Jose', 'Martinez', 'Tercer Dan');
 
-INSERT INTO Escuela VALUES(1, 'Argentina', 1);
-INSERT INTO Escuela VALUES(2, 'Bolivia', 2);
-INSERT INTO Escuela VALUES(3, 'Paraguay', 3);
+INSERT INTO Escuela VALUES(1,'Escuela 1', 'Argentina', 1);
+INSERT INTO Escuela VALUES(2,'Escuela 2', 'Bolivia', 2);
+INSERT INTO Escuela VALUES(3,'Escuela 3', 'Paraguay', 3);
 
 INSERT INTO Alumno VALUES(100000, 'Marcos', 'Perez', 1, 'Primer Dan', 'link a foto de Marcos');
 INSERT INTO Alumno VALUES(100001, 'Laura', 'Gonzalez', 2, 'Segundo Dan', 'link a foto de Laura');
@@ -191,6 +192,7 @@ INSERT INTO Modalidad VALUES(1, 'Formas', 'M');
 INSERT INTO Modalidad VALUES(2, 'Combate', 'F');
 INSERT INTO Modalidad VALUES(3, 'Combate', 'M');
 INSERT INTO Modalidad VALUES(4, 'Combate Equipos', 'M');
+INSERT INTO Modalidad VALUES(5, 'Combate', 'M');
 
 INSERT INTO Inscripcion values(1, 100003, 1);
 INSERT INTO Inscripcion values(2, 100004, 1);
@@ -209,6 +211,7 @@ INSERT INTO Competencia VALUES(1, 1);
 INSERT INTO Competencia VALUES(2, 2);
 INSERT INTO Competencia VALUES(3, 3);
 INSERT INTO Competencia VALUES(4, 4);
+INSERT INTO Competencia VALUES(5, 5);
 
 INSERT INTO Arbitro VALUES(1, 'Gonzalo', 'Perez', 'Argentina', 'Primer Dan');
 INSERT INTO Arbitro VALUES(2, 'Pepito', 'Gonzalez', 'Paraguay', 'Segundo Dan');
@@ -219,22 +222,28 @@ INSERT INTO Arbitrada VALUES(1, 1, 'Juez');
 INSERT INTO Arbitrada VALUES(2, 2, 'Presidente de Mesa');
 INSERT INTO Arbitrada VALUES(3, 3, 'Suplente');
 INSERT INTO Arbitrada VALUES(4, 4, 'Arbitro Central');
+INSERT INTO Arbitrada VALUES(4, 5, 'Arbitro Central');
 
 INSERT INTO GanaIndividualmente VALUES(1, 100003, 'Oro');
 INSERT INTO GanaIndividualmente VALUES(2, 100004, 'Oro');
 INSERT INTO GanaIndividualmente VALUES(3, 100005, 'Plata');
 INSERT INTO GanaIndividualmente VALUES(1, 100007, 'Oro');
+INSERT INTO GanaIndividualmente VALUES(2, 100007, 'Bronce');
+INSERT INTO GanaIndividualmente VALUES(3, 100007, 'Plata');
 
 INSERT INTO GanaComoEquipo VALUES(4, 1, 'Oro');
+INSERT INTO GanaComoEquipo VALUES(4, 1, 'Bronce');
+INSERT INTO GanaComoEquipo VALUES(2, 1, 'Plata');
 
+-- Ejercicio 1:
 Select  a.Nombre as Nombre, m.IdModalidad as Categoria
 From Competidor as c, Inscripcion as i, Modalidad as m, Alumno as a
 Where c.DNI = i.DNI
 and a.DNI = c.dni
 and  i.IdModalidad = m.IdModalidad
 
-
-Select res.pais, res.medalla, sum(res.cantidad) from (
+-- Ejercicio 2:
+Select res.pais as Pais, res.medalla as Medalla, sum(res.cantidad) as Cantidad from (
 (Select e.pais as Pais, gi.medalla as Medalla, count(1) as Cantidad
 From Escuela e, Maestro m, Inscripcion i, Competidor c, GanaIndividualmente as gi
 Where  e.NroDePlacaMaestro = m.NroDePlacaMaestro
@@ -253,4 +262,77 @@ and eq.idEquipo = c.idEquipo
 group by e.pais, ge.medalla) ) res
 group by res.pais, res.medalla
 
-20:31:07	Select res.pais, res.medalla, res.cantidad from ( (Select e.pais as Pais, gi.medalla as Medalla, count(1) as Cantidad From Escuela e, Maestro m, Inscripcion i, Competidor c, GanaIndividualmente as gi Where  e.NroDePlacaMaestro = m.NroDePlacaMaestro and m.NroDePlacaMaestro = i.NroDePlacaMaestro and c.dni = i.dni and gi.DNI = c.DNI group by e.pais, gi.medalla) Union All (Select e.pais as Pais, ge.medalla as Medalla, count(1) as Cantidad From Escuela e, Maestro m, Inscripcion i, Competidor c, Equipo eq, GanaComoEquipo as ge Where  e.NroDePlacaMaestro = m.NroDePlacaMaestro and m.NroDePlacaMaestro = i.NroDePlacaMaestro and c.dni = i.dni and ge.IdEquipo = eq.IdEquipo and eq.idEquipo = c.idEquipo group by e.pais, ge.medalla) ) res group by res.pais, res.medalla LIMIT 0, 1000	Error Code: 1055. Expression #3 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'res.Cantidad' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by	0.0015 sec
+-- Ejercicio 3 Por Pais:
+Select medallero.Pais, sum(medallero.Puntaje) from(
+    Select res.pais as Pais, (Case res.medalla when 'Oro' then 3 when 'Plata' then 2 else 1 end) * sum(res.cantidad) as Puntaje from (
+        (Select e.pais as Pais, gi.medalla as Medalla, count(1) as Cantidad
+        From Escuela e, Maestro m, Inscripcion i, Competidor c, GanaIndividualmente as gi
+        Where  e.NroDePlacaMaestro = m.NroDePlacaMaestro
+        and m.NroDePlacaMaestro = i.NroDePlacaMaestro
+        and c.dni = i.dni
+        and gi.DNI = c.DNI
+        group by e.pais, gi.medalla)
+    Union All
+        (Select e.pais as Pais, ge.medalla as Medalla, count(1) as Cantidad
+        From Escuela e, Maestro m, Inscripcion i, Competidor c, Equipo eq, GanaComoEquipo as ge
+        Where  e.NroDePlacaMaestro = m.NroDePlacaMaestro
+        and m.NroDePlacaMaestro = i.NroDePlacaMaestro
+        and c.dni = i.dni
+        and ge.IdEquipo = eq.IdEquipo
+        and eq.idEquipo = c.idEquipo
+        group by e.pais, ge.medalla) 
+    ) res
+    group by res.pais, res.medalla
+) medallero
+group by medallero.Pais
+
+-- Ejercicio 3 por escuela
+Select medallero.Escuela, sum(medallero.Puntaje) from(
+    select t.nombre as Escuela, (Case t.medalla when 'Oro' then 3 when 'Plata' then 2 else 1 end)*sum(t.cantidad) as Puntaje
+    from
+    ((select es.nombre, gi.medalla, count(1) as Cantidad
+    from Escuela es, GanaIndividualmente gi, Competidor c, Maestro m, Inscripcion i
+    where gi.DNI = c.DNI and m.NroDePlacaMaestro = i.NroDePlacaMaestro and i.DNI = c.DNI and es.NroDePlacaMaestro = m.NroDePlacaMaestro
+    group by es.nombre, gi.medalla
+    )
+    union
+    (select es.nombre, ge.medalla ,count(1) as Cantidad
+    from Escuela es, GanaComoEquipo ge, Competidor c, Equipo e, Maestro m, Inscripcion i
+    where ge.idEquipo = e.idEquipo and e.idEquipo=c.idEquipo and m.NroDePlacaMaestro = i.NroDePlacaMaestro and i.DNI = c.DNI and es.NroDePlacaMaestro = m.NroDePlacaMaestro
+    group by es.nombre, ge.medalla
+    )) t
+    group by t.nombre, t.medalla
+)medallero
+group by medallero.escuela
+
+-- Ejercicio 5
+select t.nombre,
+sum(case when t.medalla = 'Oro' then 1 else 0 end) as Oro,
+sum(case when t.medalla = 'Plata' then 1 else 0 end) as Plata,
+sum(case when t.medalla = 'Bronce' then 1 else 0 end) as Bronce
+from
+((select es.nombre, gi.medalla, gi.IdCompetencia
+from Escuela es, GanaIndividualmente gi, Competidor c, Maestro m, Inscripcion i
+where gi.DNI = c.DNI and m.NroDePlacaMaestro = i.NroDePlacaMaestro and i.DNI = c.DNI and es.NroDePlacaMaestro = m.NroDePlacaMaestro
+)
+union
+(select es.nombre, ge.medalla, ge.IdCompetencia
+from Escuela es, GanaComoEquipo ge, Competidor c, Equipo e, Maestro m, Inscripcion i
+where ge.idEquipo = e.idEquipo and e.idEquipo=c.idEquipo and m.NroDePlacaMaestro = i.NroDePlacaMaestro and i.DNI = c.DNI and es.NroDePlacaMaestro = m.NroDePlacaMaestro
+)) t
+group by t.nombre
+
+-- Ejercicio 6
+SELECT Nombre, Apellido, Pais FROM Arbitro
+
+-- Ejercicio 7
+select a.nombre, a.apellido 
+  from Arbitro a, arbitrada ar, competencia c, modalidad m
+    where a.NroPlacaDeArbitro = ar.NroPlacaDeArbitro and ar.IdCompetencia = c.IdCompetencia and
+        c.IdModalidad = m.IdModalidad and m.tipo = "Combate" and ar.rol = "Arbitro Central"
+
+-- Ejercicio 8
+SELECT e.Nombre, es.Pais
+FROM Equipo e, Competidor c, Inscripcion i, Maestro m, Escuela es
+WHERE e.IdEquipo = c.IdEquipo AND i.DNI = c.DNI AND m.NroDePlacaMaestro = es.NroDePlacaMaestro
+AND i.NroDePlacaMaestro = m.NroDePlacaMaestro
